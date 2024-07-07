4a98c6c2c56909c5c4c2f79d6a9f355445a3dc30
Aaron Pang <aaroncwpang@gmail.com>
2024-07-07 15:41:14 -0700

    added operator buttons and edited button layout

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
 const body = document.querySelector('body');
 const calculator = document.querySelector('.calculator');
 const inputs = document.querySelector('.inputs');
+const numbers = document.querySelector('.numbers');
+const operators = document.querySelector('.operators');
 
-for (let i = 9; i >= 0; i--) {
+const button = function(val) {
+  this.val = val;
   const num = document.createElement('button');
   num.addEventListener('click', () => {
-    alert(`${i}`);
+    alert(`${val}`);
   });
-  num.classList.add('num');
-  num.textContent = `${i}`;
-  inputs.appendChild(num);
+  
+  num.textContent = `${val}`;
+  if (num.textContent == '=') {
+    num.setAttribute('id', 'equals');
+  }
+  else {
+    num.classList.add('num');
+  }
+  return num;
 }
 
+for (let i = 9; i >= 0; i--) {
+  numbers.appendChild(new button(i));
+}
+
+numbers.appendChild(new button('.'));
+numbers.appendChild(new button('='));
+
+operators.appendChild(new button('+'));
+operators.appendChild(new button('-'))
+operators.appendChild(new button('*'))
+operators.appendChild(new button('/'))
+
+
+inputs.appendChild(numbers);
+inputs.appendChild(operators);
 calculator.appendChild(inputs);
 body.appendChild(calculator);
